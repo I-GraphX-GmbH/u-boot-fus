d495f5b0373ee529cbc08b6e9a4dcb4daae8777a
Philipp Gerbach <gerbach@fs-net.de>
2024-06-13 16:23:00 +0200

    Add CONFIG_SYS_BOOTM_LEN entries for fsimx8*
    The CONFIG_SYS_BOOTM_LEN were missing in our include files and we need
    them to boot FIT images. We already increase them from 32MB to 64MB as
    the kernel seems to get bigger with each release

--- include/configs/fsimx8mm.h
+++ include/configs/fsimx8mm.h
@@ -112,6 +112,8 @@
 
 #define CONFIG_SYS_SERCON_NAME "ttymxc"	/* Base name for serial devices */
 
+#define CONFIG_SYS_BOOTM_LEN		(64 * SZ_1M)
+
 #define CONFIG_SPL_MAX_SIZE		(140 * 1024)
 #define CONFIG_SYS_MONITOR_LEN		(512 * 1024)
 #define CONFIG_SYS_MMCSD_FS_BOOT_PARTITION	1
